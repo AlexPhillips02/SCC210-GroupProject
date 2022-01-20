899f65b3032a0a41494b772b3297bd8617966bee
k.hurst2@lancaster.ac.uk <k.hurst2@lancaster.ac.uk>
2022-01-20 13:22:16 +0000

    Memory Puzzle, shuffle method to generate a random order

--- BomberPuzzel/core/src/com/mygdx/Puzzles/Memory/ColourButton.java
+++ BomberPuzzel/core/src/com/mygdx/Puzzles/Memory/ColourButton.java
@@ -4,4 +4,11 @@ package com.mygdx.Puzzles.Memory;
  * @author Kathryn Hurst
  */
 public class ColourButton
-{}
+{
+    String name;
+
+    public ColourButton(String name)
+    {
+        this.name = name;
+    }
+}
--- BomberPuzzel/core/src/com/mygdx/Puzzles/Memory/Order.java
+++ BomberPuzzel/core/src/com/mygdx/Puzzles/Memory/Order.java
@@ -1,7 +1,34 @@
 package com.mygdx.Puzzles.Memory;
 
+import java.util.Random;
+
 /**
  * @author Kathryn Hurst
  */
 public class Order
-{}
+{
+    ColourButton R = new ColourButton("R");
+    ColourButton G = new ColourButton("G");
+    ColourButton B = new ColourButton("B");
+    ColourButton Y = new ColourButton("Y");
+
+    protected ColourButton[] Buttons = {R,G,B,Y};
+
+    public void ShuffleOrder()
+    {
+        for(int i = 0; i < Buttons.length; i++)
+        {
+            Random rand = new Random();
+            int indexSwap = rand.nextInt(Buttons.length);
+            ColourButton temp = Buttons[indexSwap];
+            Buttons[indexSwap] = Buttons[i];
+            Buttons[i] = temp;
+        }
+
+        for(int i = 0; i < Buttons.length ; i++)
+        {
+            System.out.println(Buttons[i].name);
+        }
+        System.out.println("\n");
+    }
+}
