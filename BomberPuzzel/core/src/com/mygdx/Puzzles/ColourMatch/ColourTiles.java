7fd7f3a676e17bea834d725a921325ed76384329
philli36 <alex@suremail.im>
2022-02-17 10:45:35 +0000

    Adding colour match game

--- BomberPuzzel/core/src/com/mygdx/Puzzles/ColourMatch/ColourTiles.java
+++ BomberPuzzel/core/src/com/mygdx/Puzzles/ColourMatch/ColourTiles.java
@@ -1,12 +1,124 @@
 package com.mygdx.Puzzles.ColourMatch;
 
-public class ColourTiles{
-    
-    private String colour;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.Board.Board;
 
-    public ColourTiles(String colour){
-        
+
+public class ColourTiles 
+{
+
+    protected int selectedColour;
+    protected Texture defaultImage;
+    protected float x;
+    protected float y;
+    protected Board board; 
+    protected Rectangle collisionRectangle;
+    public boolean active = true; 
+
+    public static String[] colourType = {
+        "/home/boatengv/h-drive/year2/SCC210/SCC210-GroupProject-main/BomberPuzzel/core/assets/purpleTile.jpg", //[0]
+        "/home/boatengv/h-drive/year2/SCC210/SCC210-GroupProject-main/BomberPuzzel/core/assets/greenTile.png",  //[1]
+        "/home/boatengv/h-drive/year2/SCC210/SCC210-GroupProject-main/BomberPuzzel/core/assets/orangeTile.png", //[2]
+        "/home/boatengv/h-drive/year2/SCC210/SCC210-GroupProject-main/BomberPuzzel/core/assets/redTile.png",    //[3]
+        "/home/boatengv/h-drive/year2/SCC210/SCC210-GroupProject-main/BomberPuzzel/core/assets/blueTile.png",   //[4]
+        "/home/boatengv/h-drive/year2/SCC210/SCC210-GroupProject-main/BomberPuzzel/core/assets/yellowTile.jpg"  //[5]
+    };
+
+    public ColourTiles(int colNum, Board board, float x, float y){
+        String imageURL = selectedColour(colNum);
+        this.defaultImage = new Texture(imageURL);
+        this.selectedColour = colNum;
+        this.board = board;
+        this.x = x;
+        this.y = y;   
+
+        collisionRectangle = new Rectangle(this.x, this.y, defaultImage.getWidth(), defaultImage.getHeight());    
     }
 
-}
+    private String selectedColour(int x) {
+        return colourType[x];
+    }
 
+    public boolean getActive(){
+        return this.active;
+    }
+
+    public int getselectedColourNumber(){
+        return this.selectedColour;
+    }
+
+    public void colourTouched(){
+
+    }
+
+
+    public String getColour(int x){
+        if(x == 0){
+            return "purpleTile"; 
+        }
+        if(x == 1){
+            return "greenTile";
+        }
+        if(x == 2){
+            return "orangeTile";
+        }
+        if(x == 3){
+            return "redTile";
+        }
+        if(x == 4){
+            return "blueTile";
+        }
+        if(x == 5){
+            return "yellowTile";
+        }
+
+        return "No colour";
+    }
+
+    public void Draw(SpriteBatch batch){
+        batch.draw(defaultImage, x, y);
+    }
+
+    public Texture getImage(){
+        return defaultImage;
+    }
+
+    public int getHeight(){
+        return defaultImage.getHeight();
+    }
+
+    public int getWidth(){
+        return defaultImage.getWidth();
+    }
+
+    public float getX(){
+        return x;
+    }
+
+    public float getY(){
+        return y;
+    }
+
+    public void setX(float x)
+    {
+        this.x = x;
+        collisionRectangle.x = x;
+    }
+
+    public void setY(float y)
+    {
+        this.y = y;
+        collisionRectangle.y = y;
+    }
+
+    public Rectangle getCollisionRectangle()
+    {
+        return collisionRectangle;
+    }
+
+    public void setActive(){
+        this.active = false; 
+    }
+}
--- BomberPuzzel/core/src/com/mygdx/Puzzles/ColourMatch/Match.java
+++ BomberPuzzel/core/src/com/mygdx/Puzzles/ColourMatch/Match.java
@@ -1,4 +1,83 @@
 package com.mygdx.Puzzles.ColourMatch;
 
-public class Match
-{}
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.Board.*;
+import com.mygdx.Player.Player;
+import com.mygdx.Puzzles.Puzzle;
+import com.mygdx.TileTypes.*;
+
+
+/*
+In the colour coding mini-game, there will be various colours hidden within the walls. 
+The player must walk on two of the same colour in order to complete the task. 
+When all colour combinations have completed the mini-game will be won. 
+If the Player gets into contact with another colour before completing a previous combination, 
+the colours will reset and the Player will have to start over.
+*/
+
+public class Match extends Puzzle
+{
+    ColourTiles selected_tile;
+    ArrayList<Rectangle> colourtileRectangles = new ArrayList<Rectangle>();
+
+    private static ArrayList<ColourTiles> colourTiles = new ArrayList<ColourTiles>();
+    public static int count = 0; 
+    public static boolean hasMatched = false;
+    public static ColourTiles prev;
+    public static ColourTiles current;
+
+    public Match(Board board, ArrayList<Rectangle> colourtileRectangles, ArrayList<ColourTiles> cTiles){
+        super(board);
+        this.colourtileRectangles = colourtileRectangles;
+        this.colourTiles = cTiles;
+    }
+
+    public void addColourTiles(Squares pathSquare, int colour){
+        int x = pathSquare.getX();
+        int y = pathSquare.getY();
+
+        selected_tile = new ColourTiles(colour, board, x, y);
+        colourTiles.add(selected_tile);
+        colourtileRectangles.add(selected_tile.getCollisionRectangle());
+    }
+
+    public void Draw(SpriteBatch batch){
+        for(int i = 0; i<colourTiles.size(); i++)
+        {
+            colourTiles.get(i).Draw(batch);
+        }    
+    }
+
+    public static void setCurrent(ColourTiles current_tile){
+        prev = current;
+        current = current_tile;
+        
+        if(prev != null)
+        {
+            System.out.println("pevious Coloured Tile number =" + prev.getselectedColourNumber());
+            System.out.println("current Coloured TIle number =" + current.getselectedColourNumber());
+            if(isMatch(prev, current) == true) 
+            {
+                colourTiles.remove(prev);
+                colourTiles.remove(current);
+            } else {
+                System.out.println("NO MATCH");
+            }
+        }     
+    }
+
+
+    public static boolean isMatch(ColourTiles prev, ColourTiles current) 
+    {
+        if(prev.getselectedColourNumber() == current.getselectedColourNumber())
+        {
+            return true;
+        } else {
+            return false; 
+        }
+    }
+}
