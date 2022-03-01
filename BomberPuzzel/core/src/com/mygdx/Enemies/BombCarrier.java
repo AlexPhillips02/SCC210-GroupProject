19e0941c0f53b200393af40e87a4849dcb95b651
Alex Phillips <Alex@suremail.im>
2022-03-01 11:43:13 +0000

    Adding new images

--- BomberPuzzel/core/src/com/mygdx/Enemies/BombCarrier.java
+++ BomberPuzzel/core/src/com/mygdx/Enemies/BombCarrier.java
@@ -11,7 +11,7 @@ public class BombCarrier extends PlayerTrackingEnemy
 
     public BombCarrier(Board board, float x, float y, float movementSpeed, Player player)
     {
-        super("BombExplosionThings/pixil-frame-0.png", board, x, y, movementSpeed, player, 10);
+        super("Enemies/Spider enemy/spider(single).png", board, x, y, movementSpeed, player, 10);
         this.health = 1;
     }
 
